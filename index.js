6bb33a7c1dc06cae216206958a693b7b26c09228
AdolfoAriel <arielnegre@gmail.com>
2023-11-23 12:50:59 -0600

    commit

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ import express from 'express';
 
 const app = express()
 
-app.listen(5001, () => console.log("Api tunning in the PORT 5001"))
+app.listen(5001, () => console.log("Hola Sofia, te amo mucho!!"))
 
 app.get('/', (req,res) => res.json("My API is running"))
